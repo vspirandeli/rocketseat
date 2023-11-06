45ca40adf9b844697cbd4cfb7c9b4fbe56fd0df2
Victor Spirandeli <vspirandeli@gmail.com>
2023-11-05 21:39:13 -0300

    feat(parte-2): Aula: Componente Sidebar. Criação do componente Sidebar

--- ignite/reactJS/2023/projeto01/01-fundamentos-reactjs/src/App.jsx
+++ ignite/reactJS/2023/projeto01/01-fundamentos-reactjs/src/App.jsx
@@ -1,12 +1,20 @@
 import { Header } from './components/Header';
 
 import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar';
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
-    </>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main></main>
+      </div>
+    </div>
   )
 }
 
---
+++ ignite/reactJS/2023/projeto01/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1603468620905-8de7d86b781e?auto=format&fit=crop&q=50&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+        alt="No alt :(" 
+      />
+
+      <div className={styles.profile}>
+        <strong>Victor Spirandeli</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">Editar seu perfil</a>
+      </footer>
+    </aside>
+  )
+}

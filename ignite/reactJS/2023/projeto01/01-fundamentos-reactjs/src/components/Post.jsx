c395513db4d34c75a5d9143068f5241455662dfe
Victor Spirandeli <vspirandeli@gmail.com>
2023-11-08 00:32:22 -0300

    feat(parte-2): Os Motores do React. Aula: Propriedades do Post. Adicionado os parâmetros dentro do componente Post

--- ignite/reactJS/2023/projeto01/01-fundamentos-reactjs/src/components/Post.jsx
+++ ignite/reactJS/2023/projeto01/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,12 +1,15 @@
+/* eslint-disable react/prop-types */
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 
 import styles from './Post.module.css';
 
 // TODO: Melhorar o post vazio
-export function Post(props) {
-  // eslint-disable-next-line react/prop-types
-  if (!props?.author) {
+export function Post({ author, publishedAt, content }) {
+  if (!author) {
     return emptyPost();
   }
 
@@ -20,31 +23,38 @@ export function Post(props) {
     )
   }
 
+  const publishedDateFormated = format(publishedAt, "d LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/vspirandeli.png" />
+          <Avatar src={author.avatarUrl} />
 
           <div className={styles.authorInfo}>
-            <strong>Victor Spirandeli</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="11 de Maio às 08:11h" dateTime="2023-05-11 08:11:08">Publicado há 1h</time>
+        <time title={publishedDateFormated} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-        <p><a href="">jane.design/doctorcare</a></p>
-        <p>
-          <a href="">#novoprojeto</a>{" "}
-          <a href="">#nlw</a>{" "}
-          <a href="">#rocketseat</a>
-        </p>
+        {content.map(item => {
+          if (item.type === 'paragraph') {
+            return <p key={item.content}>{item.content}</p>
+          } else if (item.type === 'link') {
+            return <p key={item.content}><a href="">{item.content}</a></p>
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>

40388db3a792aefaeed220292b6164476be77cba
Victor Spirandeli <vspirandeli@gmail.com>
2023-11-27 22:54:47 -0300

    feat(parte-2): Os Motores do React. Aula: Estado (useState). Criado estado para comentários.

--- ignite/reactJS/2023/projeto01/01-fundamentos-reactjs/src/components/Post.jsx
+++ ignite/reactJS/2023/projeto01/01-fundamentos-reactjs/src/components/Post.jsx
@@ -6,9 +6,13 @@ import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 
 import styles from './Post.module.css';
+import { useState } from 'react';
+
 
 // TODO: Melhorar o post vazio
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([1, 2]);
+
   if (!author) {
     return emptyPost();
   }
@@ -31,6 +35,12 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBR,
     addSuffix: true,
   });
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+
+    setComments([...comments, comments.length + 1])
+  }
 
   return (
     <article className={styles.post}>
@@ -57,7 +67,7 @@ export function Post({ author, publishedAt, content }) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea
@@ -70,10 +80,9 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => (
+          <Comment key={comment} />
+        ))}
       </div>
     </article>
   )
